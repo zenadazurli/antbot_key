5927e74d6d720b8a80d2a29431d0c42689d85bc0
zenadazurli <zenadazurli@libero.it>
2026-08-03 00:27:40 +0200

    Update config.py

--- config.py
+++ config.py
@@ -5,11 +5,11 @@
 
 # 🔑 10 KEY PER QUESTO BOT
 BOT_KEYS = [
-    "",
-    "",
-    "",
-    "",
-    "",
+    "bu_RbMquV7YR4NE1iN4trgwDaFelu73i8yg2ubMZyiozhw",
+    "bu_3fYwgVJcyVRNnae1F7WD7-oFUtaRU6pxn91VcjzcKJ0",
+    "bu_b5nxWw-9nV8vn84vZ0aTtFANvkYPKZwrAVet0wxVMe4",
+    "bu_ApNWFqyJ7LEgDZtk6IFzOaJO5CJwY4_xvrTMUt72Gac",
+    "bu_UU1QcKnlt04xtCdoNvO-HMTZy9VCivLFgCD2Eec0lig",
     "",
     "",
     "",
