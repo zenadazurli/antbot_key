99c90edf917b2a54b48c4fde5622bc8fcaae4b71
zenadazurli <zenadazurli@libero.it>
2026-08-03 00:20:04 +0200

    Update config.py

--- config.py
+++ config.py
@@ -5,16 +5,16 @@
 
 # 🔑 10 KEY PER QUESTO BOT
 BOT_KEYS = [
-    "bu_vpqaFMR7fG2iGto2Z9oLXXL8M3Fkm25Y87_BwKp2aAU",
-    "bu_Ka06jkNsi1vGfaYE7aj4CAW77tpWf6BGQXR18nlpBKg",
-    "bu_SbBc9AEjdyWD_J1WknhcwdDh6sCKXL2xMv5ygjBORtU",
-    "bu__OQHbfU22mmZe0owTw8TMt0bnl2nzK8K5cnn5_5uByc",
-    "bu_bFU_BByr5Xc2-4TFQsP3vx1w52mUEZdzuIBOh0djioU",
-    "bu_hzdxRQ384RsYb03CgF2PyBbYYlFno90KRs7ti0TJU7s",
-    "bu_6Y5N__7khOFC9me8MtuvMIjALXy9jYIpgxMmEmMNQW8",
-    "bu_n4aY69LLzE1zW5wY6XTAl8NsJP4oqg46lrgrj3rX6dA",
-    "bu_iU6nblRffmff1ffAL2HOpqpychKxWaNSO2NzEo8Fzso",
-    "bu_D0CiVbpU1NWs8UGfVhQyLNGfGCF09ttS_UhzmHmK0U0",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
 ]
 
 # ⚙️ IMPOSTAZIONI
