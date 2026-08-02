34dfe00c8131a6f9dfc593c28a435e7fd980ca31
zenadazurli <zenadazurli@libero.it>
2026-08-03 00:29:11 +0200

    Update config.py

--- config.py
+++ config.py
@@ -10,11 +10,11 @@ BOT_KEYS = [
     "bu_b5nxWw-9nV8vn84vZ0aTtFANvkYPKZwrAVet0wxVMe4",
     "bu_ApNWFqyJ7LEgDZtk6IFzOaJO5CJwY4_xvrTMUt72Gac",
     "bu_UU1QcKnlt04xtCdoNvO-HMTZy9VCivLFgCD2Eec0lig",
-    "",
-    "",
-    "",
-    "",
-    "",
+    "bu_7FTcwPLzHukWVyjADlyCA9ZdjJ1sRN_sHZSe5tDKnik",
+    "bu_2CEWcFvHIcxWm1jEPU078aD0Ena5Eh62cxtQku4iINg",
+    "bu_6DerBJ1GKbA773TRQA9Tz6bMrL-DFzkA7hh6QhGmjr0",
+    "bu_ot0CVHIk34liC2dVyq1CpO62zpus5-ILKRFYbUIXLEo",
+    "bu_DpKJKFWb80Y3tthBGdZ6aOSek5NtRl-b_DvLm7Gc6c4",
 ]
 
 # ⚙️ IMPOSTAZIONI
